ad817a7b72af970e90a341794a360419def3fe97
Othman <othman.douz07@gmail.com>
2023-05-30 16:28:40 +0200

    test

--- script.js
+++ script.js
@@ -27,22 +27,22 @@ const questions = [
   // Fonction appelée lors de la soumission du formulaire
   function submitAnswer(event) {
     event.preventDefault();
-    
+  
     const answerInput = document.getElementById("answer");
     const answer = answerInput.value.trim();
-    
+  
     if (answer === questions[currentQuestion].answer) {
       showResult(true);
     } else {
       showResult(false);
     }
-    
+  
     attempts++;
-    
+  
     if (attempts >= 3) {
       showHint();
     }
-    
+  
     answerInput.value = "";
     answerInput.focus();
   }
@@ -51,7 +51,7 @@ const questions = [
   function showResult(isCorrect) {
     const resultElement = document.getElementById("result");
     const feedbackElement = document.getElementById("feedback");
-    
+  
     if (isCorrect) {
       resultElement.style.color = "green";
       resultElement.textContent = "Correct!";
@@ -61,7 +61,7 @@ const questions = [
       resultElement.textContent = "Incorrect!";
       feedbackElement.textContent = "Essayez encore.";
     }
-    
+  
     if (currentQuestion < questions.length - 1) {
       currentQuestion++;
       displayQuestion();
@@ -76,7 +76,24 @@ const questions = [
     const hintElement = document.getElementById("hint");
     hintElement.textContent = questions[currentQuestion].hint;
   }
+
+// Fonction pour effectuer une requête HTTP GET vers l'API League of Legends
+async function fetchChampionRotations() {
+    const response = await fetch("https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-b0eca0be-40d5-4cf1-94ff-158c3acae8d2");
+    const data = await response.json();
+    return data;
+  }
   
-  // Appel initial pour afficher la première question
-  displayQuestion();
+  // Fonction pour afficher les rotations de champions
+  async function displayChampionRotations() {
+    const championRotations = await fetchChampionRotations();
+    console.log(championRotations); // Affiche les données des rotations de champions dans la console
+  
+    // Vous pouvez maintenant utiliser les données pour afficher les rotations de champions dans votre interface utilisateur
+    // par exemple, en mettant à jour les éléments HTML appropriés avec les informations récupérées
+    // Assurez-vous de modifier cette fonction en fonction de votre structure HTML et de la manière dont vous souhaitez afficher les données.
+  }
+  
+  // Appel initial pour afficher les rotations de champions
+  displayChampionRotations();
   
